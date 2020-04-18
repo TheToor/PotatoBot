ea3ff25ffa704f9c3ff09f75598aa8e1c21681d6
Gérald Döserich <github@doeseri.ch>
2020-04-18 20:53:29 +0200

    Added Version command

---
+++ PotatoBot/Commands/VersionCommand.cs
@@ -0,0 +1,17 @@
+﻿using PotatoBot.Modals.Commands;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PotatoBot.Commands
+{
+    [Command("version")]
+    internal class VersionCommand : Service, ICommand
+    {
+        public async Task<bool> Execute(TelegramBotClient client, Message message, string[] arguments)
+        {
+            await TelegramService.SimpleReplyToMessage(message, $"Version {Program.Version} of {Program.Namespace}");
+            return true;
+        }
+    }
+}
