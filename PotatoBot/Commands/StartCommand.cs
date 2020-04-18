0f001be708266021e6d25d74bfa5c7e992346eb1
Gérald Döserich <github@doeseri.ch>
2020-04-18 20:56:37 +0200

    Added Start Command

---
+++ PotatoBot/Commands/StartCommand.cs
@@ -0,0 +1,17 @@
+﻿using PotatoBot.Modals.Commands;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PotatoBot.Commands
+{
+    [Command("start")]
+    internal class StartCommand : Service, ICommand
+    {
+        public async Task<bool> Execute(TelegramBotClient client, Message message, string[] arguments)
+        {
+            await TelegramService.SimpleReplyToMessage(message, LanguageManager.GetTranslation("Commands", "Start", "Start"));
+            return true;
+        }
+    }
+}
