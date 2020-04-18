2c9d1ef814bc8ce7b2240153cf856dd272b072fb
Gérald Döserich <github@doeseri.ch>
2020-04-18 20:59:48 +0200

    Added Help Command

---
+++ PotatoBot/Commands/HelpCommand.cs
@@ -0,0 +1,17 @@
+﻿using PotatoBot.Modals.Commands;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PotatoBot.Commands
+{
+    [Command("help")]
+    internal class HelpCommand : Service, ICommand
+    {
+        public async Task<bool> Execute(TelegramBotClient client, Message message, string[] arguments)
+        {
+            await TelegramService.SimpleReplyToMessage(message, LanguageManager.GetTranslation("Commands", "Help", "Help"));
+            return true;
+        }
+    }
+}
