1fc43a6769cbcf195b7f2b764aada8a41728c1b5
Gérald Döserich <github@doeseri.ch>
2020-08-02 22:05:15 +0200

    Marked services as Static

--- src/PotatoBot/Modals/Commands/Service.cs
+++ src/PotatoBot/Modals/Commands/Service.cs
@@ -5,10 +5,10 @@ namespace PotatoBot.Modals.Commands
 {
     internal class Service
     {
-        internal LanguageManager LanguageManager => Program.LanguageManager;
-        internal TelegramService TelegramService => Program.ServiceManager.TelegramService;
-        internal SonarrService SonarrService => Program.ServiceManager.Sonarr;
-        internal RadarrService RadarrService => Program.ServiceManager.Radarr;
-        internal LidarrService LidarrService => Program.ServiceManager.Lidarr;
+        internal static LanguageManager LanguageManager => Program.LanguageManager;
+        internal static TelegramService TelegramService => Program.ServiceManager.TelegramService;
+        internal static SonarrService SonarrService => Program.ServiceManager.Sonarr;
+        internal static RadarrService RadarrService => Program.ServiceManager.Radarr;
+        internal static LidarrService LidarrService => Program.ServiceManager.Lidarr;
     }
 }
