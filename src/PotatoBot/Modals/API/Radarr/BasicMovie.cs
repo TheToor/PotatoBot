ac518f251c62257796be0a1e0fbad5a3a1ea0254
Gérald Döserich <github@doeseri.ch>
2020-08-04 21:26:09 +0200

    Added Studio and Runtime to BasicMovie

--- src/PotatoBot/Modals/API/Radarr/BasicMovie.cs
+++ src/PotatoBot/Modals/API/Radarr/BasicMovie.cs
@@ -7,12 +7,16 @@ namespace PotatoBot.Modals.API.Radarr
         public string Title { get; set; }
         public long SizeOnDisk { get; set; }
         public Image Poster { get; set; }
+        public string Studio { get; set; }
+        public ushort Runtime { get; set; }
 
         public BasicMovie(Movie movie)
         {
             Title = movie.Title;
             SizeOnDisk = movie.SizeOnDisk;
             Poster = movie.Images?.FirstOrDefault((i) => i.CoverType == "poster");
+            Studio = movie.Studio;
+            Runtime = movie.Runtime;
         }
     }
 }
