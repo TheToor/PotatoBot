b41787ee7b0ecc59e247bfd196cdcbb8d1a8b523
Gérald Döserich <github@doeseri.ch>
2022-02-12 19:48:31 +0100

    Added missing files

---
+++ src/PotatoBot/Modals/API/Plex/Release/ReleaseContainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+
+namespace PotatoBot.Modals.API.Plex.Release
+{
+    [XmlRoot(ElementName = "MediaContainer")]
+    public class ReleaseContainer
+    {
+        [XmlAttribute(AttributeName = "size")]
+        public ulong Size { get; set; }
+        [XmlAttribute(AttributeName = "canInstall")]
+        public bool CanInstall { get; set; }
+        [XmlAttribute(AttributeName = "checkedAt")]
+        public ulong CheckedAt { get; set; }
+        [XmlAttribute(AttributeName = "downloadURL")]
+        public string DownloadUrl { get; set; }
+        // Unknown
+        [XmlAttribute(AttributeName = "status")]
+        public byte Status { get; set; }
+
+        [XmlElement(ElementName = "Release")]
+        public Release Release { get; set; }
+    }
+}
